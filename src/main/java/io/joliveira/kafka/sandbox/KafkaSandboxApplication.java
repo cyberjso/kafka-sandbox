a42fed1bf81a6ca79eeed4b8eeec08f2f4ab44fd
jackson.oliveira <jackson.oliveira@ilegra.com>
2023-04-07 22:13:50 -0300

    first commit

---
+++ src/main/java/io/joliveira/kafka/sandbox/KafkaSandboxApplication.java
@@ -0,0 +1,11 @@
+package io.joliveira.kafka.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSandboxApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSandboxApplication.class, args);
+	}
+}
